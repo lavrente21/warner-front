027adf74c38b56e1c0a479f6e7a7e2ae437d8b59
Lavrente Mambondo <lavrentemambondo@gmail.com>
2026-01-13 20:21:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -43,12 +43,15 @@ async function login() {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email, senha })
         });
-        const user = await res.json();
-        if (res.ok) {
-            localStorage.setItem('usuario', JSON.stringify(user));
+
+        const data = await res.json();
+
+        if(res.ok) {
+            // SALVA SEMPRE COMO 'usuario' PARA COMBINAR COM O INDEX
+            localStorage.setItem('usuario', JSON.stringify(data));
             window.location.href = 'index.html';
         } else {
-            toast(user.error || "Dados incorretos");
+            toast(data.error || "Dados incorretos");
         }
     } catch (err) { toast("Erro de conexão"); }
 }
